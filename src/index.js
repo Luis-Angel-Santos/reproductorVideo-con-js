5def4a9cc440cf3a4bfa9d9d0129eb6eda9bbb88
Luis Angel Santos <santosangel748@gmail.com>
2022-02-09 17:53:26 -0600

    Creando reproductor de video con controles personalizados

--- src/index.js
+++ src/index.js
@@ -1,6 +1,38 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+var video = document.getElementById('video');
+var play = document.getElementById('iniciar');
+var load = document.getElementById('recargar');
+var max = document.getElementById('maximizar');
+var silence = document.getElementById('silenciar');
+var volumen = document.getElementById('volumen');
+var volumenS = document.getElementById('volumenS');
+video.volume = 1;
 
-console.log('Happy hacking :)')
+play.addEventListener('click', () => {
+    if (video.paused) 
+        video.play(); 
+    else 
+        video.pause(); 
+});
+
+load.addEventListener('click', () => {
+    video.load(); 
+});
+
+max.addEventListener('click', () => {
+    video.requestFullscreen(); 
+})
+
+silence.addEventListener('click', () => {
+    if(video.muted == false)
+        video.muted = true;
+    else
+        video.muted = false;  
+})
+
+volumenS.addEventListener('click', () => {
+    video.volume = 1;
+})
+
+volumen.addEventListener('click', () => {
+    video.volume = 0.1;
+})
